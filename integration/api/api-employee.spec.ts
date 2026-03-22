f6c904b212be695a4b47b9ddff59cc34786d179a
kubecloudsinc <kubecloudsinc@gmail.com>
2026-03-22 18:27:47 -0400

    add negative tests for employee

---
+++ integration/api/api-employee.spec.ts
@@ -0,0 +1,22 @@
+import { container } from '@support/containers';
+import {
+  EMPLOYEE_SYMBOLS,
+  GetEmployeeResponse,
+  IEmployeeAPI,
+} from '@shared/employee';
+import { ApiError } from '@shared/base';
+
+describe('Test Employee APIs', () => {
+  let _employeeAPI: IEmployeeAPI;
+
+  before(() => {
+    _employeeAPI = container.get(EMPLOYEE_SYMBOLS.IEmployeeAPI);
+  });
+  it('should get error response for invalid employee id', () => {
+    _employeeAPI.getEmployee('100,').then((response: GetEmployeeResponse) => {
+      const error = response as ApiError;
+      expect(error.status).to.eq(400);
+
+    });
+  });
+});
